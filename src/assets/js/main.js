167592391c303a3a98b172c8ff6884b1130283c0
fery yundara putera <fery@xtremax.com>
2018-02-12 14:19:13 +0700

    try #1

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -5,10 +5,10 @@ $( document ).ready(function() {
     for (var i = 0; i < men.length; i++) {
 	    if(men[i].child != null){
 	    	var kl='<a class="dropdown-button" href="#" data-activates="'+men[i].menu_id+'" dropdown data-hover="true">'+men[i].text_menu+'<i class="material-icons right">arrow_drop_down</i></a>';
-    		var klmobile = '<a class="dropdown-button" href="#" data-activates="'+men[i].menu_id+'">'+men[i].text_menu+'<i class="material-icons right">arrow_drop_down</i></a>'
     		$('#dropdown-'+men[i].menu_id).html(kl);
-    		$('#dropdown-mobile-'+men[i].menu_id).html(klmobile);
-	    }
+        $('#item_'+men[i].menu_id).html('Solutions<i class="material-icons right">arrow_drop_down</i>').
+        attr('data-activates', 'data_'+men[i].menu_id);
+      }
     }
 	$('.dropdown-button').dropdown({
       inDuration: 300,
